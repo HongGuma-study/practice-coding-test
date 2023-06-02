f8d92300e2d6d6e9187bd5000bc92dfdf45442b0
HongGuma <hongsu9849@gmail.com>
2023-06-03 00:44:35 +0900

    2023.06.02 프로그래머스 배열 조각하기 문제 풀이

---
+++ src/programmers/level00/FragmentingAnArray.java
@@ -0,0 +1,42 @@
+package programmers.level00;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FragmentingAnArray {
+    public static void main(String[] args) {
+        solution(new int[]{0,1,2,3,4,5}, new int[]{4,1,2}); // 1,2,3
+        solution(new int[]{0,1,2,3,4,5,6,7}, new int[]{4,1,2}); // 1,2,3
+    }
+
+    public static ArrayList<Integer> solution(int[] arr, int[] query) {
+        ArrayList<Integer> answer = new ArrayList<>();
+       
+        for(int a : arr){
+            answer.add(a);
+        }
+        
+        int num = 0;
+
+        for(int q : query){
+            if(num % 2 == 0){ //짝수 인덱스
+                //뒤에 자르기
+                int idx = answer.size() - 1;
+                for(int i = idx ; i > q ; i--){
+                    answer.remove(i);
+                }
+            }else{ //홀수 인덱스
+                //앞에 자르기
+                for(int i = 0 ; i < q ; i++){
+                    answer.remove(0);
+                }
+            }
+            num++;
+        }
+    
+        System.out.println(answer);
+        return answer;
+    }
+    
+}
