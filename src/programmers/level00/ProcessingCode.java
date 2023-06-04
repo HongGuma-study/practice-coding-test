401fa033afce18e48459d59a9f0202cdc87773d7
HongGuma <hongsu9849@gmail.com>
2023-06-04 15:45:55 +0900

    2023.06.04 프로그래머스 코드 처리하기 문제 풀이

---
+++ src/programmers/level00/ProcessingCode.java
@@ -0,0 +1,33 @@
+package programmers.level00;
+
+class ProcessingCode{
+    public static void main(String[] args) {
+        solution("abc1abc1abc");
+    }
+
+    public static String solution(String code){
+        String answer = "";
+
+        boolean mode = false; //true 1 false 0
+
+        for(int i=0 ; i < code.length() ; i++){
+            if(code.charAt(i) == '1'){
+                mode = !mode;
+                continue;
+            }
+
+            if(mode){ // mode == 1;
+                if(i % 2 != 0){
+                    answer += code.charAt(i);
+                }
+            }else{ // mode == 0;
+                if(i % 2 == 0){
+                    answer += code.charAt(i);
+                }
+            }
+        }
+
+        System.out.println(answer);
+        return answer;
+    }
+}
