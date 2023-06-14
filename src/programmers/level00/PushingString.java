4b6d57e7504c5ee264b6def669e185e74fc265e0
HongGuma <hongsu9849@gmail.com>
2023-06-14 18:06:01 +0900

    2023.06.14 프로그래머스 '문자열 밀기' 문제 풀이

---
+++ src/programmers/level00/PushingString.java
@@ -0,0 +1,26 @@
+package programmers.level00;
+
+class PushingString{
+    public static void main(String[] args) {
+        solution("hello", "ohell"); //1
+        solution("apple", "elppa"); //-1
+        solution("atat","tata"); //1
+        solution("abc","abc"); //0
+        
+    }
+
+    public static int solution(String A, String B) {
+        int answer = 0;
+
+        for(int i = 0 ; i < A.length() ; i++){
+            if(A.equals(B)) 
+                return answer;
+            String last = A.substring(A.length()-1);
+            A = A.substring(0, A.length() - 1);
+            A = last+A;
+            answer++;
+        }
+            
+        return -1;
+    }
+}
