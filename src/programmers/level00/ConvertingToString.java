77adfd1e1093f734c11d79b303f38efd5429105f
HongGuma <hongsu9849@gmail.com>
2024-02-06 14:32:50 +0900

    2024.02.06 프로그래머스 '문자열로 변환' 문제 풀이

---
+++ src/programmers/level00/ConvertingToString.java
@@ -0,0 +1,13 @@
+package programmers.level00;
+
+public class ConvertingToString {
+    public static void main(String[] args) {
+        
+    }
+
+    public String solution(int n) {
+        String answer = "";
+        answer += n;
+        return answer;
+    }
+}
