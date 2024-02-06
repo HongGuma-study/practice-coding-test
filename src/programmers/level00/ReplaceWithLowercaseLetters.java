99315827e4e7dd977830c877ee258d26eeb77818
HongGuma <hongsu9849@gmail.com>
2024-02-06 14:34:53 +0900

    2024.02.06 프로그래머스 '소문자로 바꾸기' 문제 풀이

---
+++ src/programmers/level00/ReplaceWithLowercaseLetters.java
@@ -0,0 +1,11 @@
+package programmers.level00;
+
+public class ReplaceWithLowercaseLetters {
+    public String solution(String myString) {
+        String answer = "";
+        
+        answer = myString.toLowerCase();
+        
+        return answer;
+    }
+}
