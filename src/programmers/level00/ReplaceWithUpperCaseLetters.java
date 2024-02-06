4a64b93307d376e11c5f2853d345a1b128f411fc
HongGuma <hongsu9849@gmail.com>
2024-02-06 14:37:19 +0900

    2024.02.06 프로그래머스 '대문자로 바꾸기' 문제 풀이

---
+++ src/programmers/level00/ReplaceWithUpperCaseLetters.java
@@ -0,0 +1,9 @@
+package programmers.level00;
+
+class ReplaceWithLowercaseLetters {
+    public String solution(String myString) {
+        String answer = "";
+        answer = myString.toUpperCase();
+        return answer;
+    }
+}
